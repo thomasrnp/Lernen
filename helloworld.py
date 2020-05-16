1ba47d2547528271887bf44b5f0b3cd1f66c5921
Thomas Glatte <thomas_rnp@web.de>
2020-05-16 14:28:55 +0200

    jetson nano Lesson 5: 1:08:28

---
+++ helloworld.py
@@ -0,0 +1,42 @@
+# import numpy as np
+
+print("Hello World")
+# myNum=int(input("Please input a number: "))
+# for i in range(0,myNum,1):
+#     print(i)
+
+
+# x=7
+# y=3
+# z=x/y
+# Formatierung von Zahlen
+# print(x, " / ", y, " = ", "{:.2f}".format(z))
+
+# number=float(input("Please input your number: "))
+# if (number > 0):
+#     print("Your number is positiv.")
+# if (number < 0):
+#     print("Your number is negativ.")
+
+# for i in np.arange(-.5,.5,.1):
+#     print(i)
+# print("That's all folks.")
+
+# for i in np.linspace(1,10,10):
+#     print(i)
+# print("That's all folks")
+
+# while (1==1):
+#     cnt=float(input("What do you want to count to: "))
+#     j=1
+#     while j<=cnt:
+#         print("{:.2f}".format(j))
+#         j=j+1
+#     print("That's all folks.")
+
+gradeArray=[]
+gradeArray.append(5.5)
+gradeArray.append(3.2)
+gradeArray.append(-2.7)
+gradeArray[1]=255.5
+print(gradeArray)

0a98e973430f538dc24cfcbd305910b88ad5b17c
Thomas Glatte <thomas_rnp@web.de>
2020-05-16 18:59:31 +0200

    Jetson Nano Lesson 11: 19:04

---
+++ openCVintro.py
@@ -0,0 +1,12 @@
+import cv2
+
+cam = cv2.VideoCapture(0)
+while True:
+    ret, frame=cam.read()
+    cv2.imshow('Kamera', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cam.release()
+cv2.destroyAllWindows
+
+

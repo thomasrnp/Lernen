635da0428af4044b632397804916897eace83eba
Thomas Glatte <thomas_rnp@web.de>
2020-05-18 21:25:06 +0200

    Jetson Nano Lesson 12: 24:35

--- openCV2_move_window.py
+++ openCV2_move_window.py
@@ -5,6 +5,11 @@ cam = cv2.VideoCapture(0)
 while True:
     ret, frame=cam.read()
     cv2.imshow('Kamera', frame)
+    cv2.moveWindow('Kamera',0,0)
+    
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    cv2.imshow('GrayVideo', gray)
+    cv2.moveWindow('GrayVideo',0,520)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cam.release()

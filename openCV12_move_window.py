4ecc2f19c630961475730612688152fcb0d2dd85
Thomas Glatte <thomas_rnp@web.de>
2020-06-14 14:07:13 +0200

    CV12 mit mp4

--- openCV12_move_window.py
+++ openCV12_move_window.py
@@ -1,16 +1,21 @@
 import cv2
 
-cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture(0)
+cam = cv2.VideoCapture('/home/fred/Videos/Thomas.mp4')
 
-while True:
-    ret, frame=cam.read()
+# Check if camera opened successfully
+if (cam.isOpened()== False): 
+    print("Error opening video stream or file!")
+
+while(cam.isOpened()):
+    ret, frame = cam.read()
     cv2.imshow('Kamera', frame)
     cv2.moveWindow('Kamera',0,0)
     
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
     cv2.imshow('GrayVideo', gray)
     cv2.moveWindow('GrayVideo',0,520)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(25) & 0xFF == ord('q'):
         break
 cam.release()
 cv2.destroyAllWindows
